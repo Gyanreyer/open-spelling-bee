f27539483e038b66cd39409052cb735137502363
Ryan <gyanreyer@gmail.com>
2023-04-07 18:15:47 -0400

    Fix score rank modal styles

--- src/pwa/sw.js
+++ src/pwa/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "open-spelling-bee-0.10.9";
+const cacheName = "open-spelling-bee-1.0.0";
 const cacheFiles = ["/index.html", "/js/main.mjs"];
 
 self.addEventListener("install", (e) => {
